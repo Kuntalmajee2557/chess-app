845f13c07d45dcfb9af7cd6165bf9ae66d82f732
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2024-12-18 20:52:59 +0530

    Game and GameManager class method initialized

--- backend/src/GameManager.ts
+++ backend/src/GameManager.ts
@@ -1,22 +1,45 @@
+import { INIT_GAME, MOVE } from "./message";
 
 
 export class GameManager {
-    private games: Game[]
+    private games: Game[];
+    private pendingUser: WebSocket | null;
+    private users: WebSocket[];
     
     constructor(){
         this.games = [];
+        this.users = [];
+        this.pendingUser = null;
     }
 
     addUser(socket: WebSocket){
-
+        this.users.push(socket);
     }
 
     removeUser(socket: WebSocket){
-
+        this.users = this.users.filter(user => user != socket);
     }
 
-    private handleMassage(){
-        
+    private handleMassage(socket: WebSocket){
+        socket.on("message", (data: any) => {
+            const message = JSON.parse(data.type);
+
+            if(message === INIT_GAME){
+                if(this.pendingUser){
+                    const game = new Game(this.pendingUser, socket);
+                    this.games.push(game);
+                    this.pendingUser = null;
+                }
+                else{
+                    this.pendingUser = socket;
+                }
+            }
+
+            if(message === MOVE){
+                const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
+                game?.makeMove(socket, message.move);
+            }
+        })
     }
 
 
